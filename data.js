9a5e1835e87e9fe1c2c11bfd07684f719bc61f7f
Lalwani-Karishma <lalwanikarishma216@gmail.com>
2024-05-03 17:00:28 +0530

    1.0

---
+++ data.js
@@ -0,0 +1,10 @@
+
+let a="HOME";
+let b="ABOUT";
+let c="SERVICES";
+let d="CONTACT";
+
+document.getElementById('h1').innerHTML=a;
+document.getElementById('h2').innerHTML=b;
+document.getElementById('h3').innerHTML=c;
+document.getElementById('h4').innerHTML=d;
